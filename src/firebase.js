f5125c1536faea45f7da208a12c19a01b2bbc398
Yee Chin, Chiam <yeechin.chiam@gmail.com>
2023-09-24 15:55:31 -0700

    disable analytic

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
 import { initializeApp } from "firebase/app";
 import { Timestamp, getFirestore } from "firebase/firestore";
 import dayjs from "dayjs";
